28f0a0d0a621786e310c4da46e7db332edb5f829
kms0917 <5670kms@gmail.com>
2024-04-02 14:53:36 +0900

    Gameview 골자 만들기 1차

--- TexTris/src/main/java/org/Stech/SE5/View/GameView.java
+++ TexTris/src/main/java/org/Stech/SE5/View/GameView.java
@@ -1,7 +1,11 @@
 package org.Stech.SE5.View;
 
 import javax.swing.*;
+import javax.swing.JComponent;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.text.SimpleAttributeSet;
+import java.awt.*;
 
 public class GameView extends JFrame {
 
@@ -25,9 +29,76 @@ public class GameView extends JFrame {
         setSize(VIEW_WIDTH, VIEW_HEIGHT);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        backgroundPanel.setLayout(null);
+        backgroundPanel.setBorder(BorderFactory.createEmptyBorder(200, 0, 0, 25));
+        add(backgroundPanel);
+
+        JPanel nextPanel = new JPanel();
+        nextPanel.setLayout(null);
+        nextPanel.setBounds(280, 54, 70, 120);
+        nextPanel.setBackground(Color.GRAY); // 패널의 배경색을 회색으로 설정
+
+        JLabel nextLabel = new JLabel("next", SwingConstants.CENTER);    // nextPanel에 "next" 텍스트를 추가합니다.
+        nextLabel.setForeground(Color.WHITE); // 텍스트 색상을 설정합니다.
+        nextLabel.setBounds(0, 0, 70, 20); // 라벨의 위치와 크기를 설정합니다.
+        nextPanel.add(nextLabel);
+        backgroundPanel.add(nextPanel);
+
+        JPanel scorePanel = new JPanel();
+        scorePanel.setLayout(null);
+        scorePanel.setBounds(280,260,70,75);
+        scorePanel.setBackground(Color.GRAY);
+
+        JLabel scoreLabel = new JLabel("score", SwingConstants.CENTER); // scorePanel에 "score" 텍스트를 추가합니다.
+        scoreLabel.setForeground(Color.WHITE);
+        scoreLabel.setBounds(0, 0, 70, 20);
+        scorePanel.add(scoreLabel);
+        backgroundPanel.add(scorePanel);
+
+        JPanel levelPanel = new JPanel();
+        levelPanel.setLayout(null);
+        levelPanel.setBounds(280,340,70,75);
+        levelPanel.setBackground(Color.GRAY);
+
+        JLabel levelLabel = new JLabel("level", SwingConstants.CENTER); // levelPanel에 "level" 텍스트를 추가합니다.
+        levelLabel.setForeground(Color.WHITE);
+        levelLabel.setBounds(0, 0, 70, 20);
+        levelPanel.add(levelLabel);
+        backgroundPanel.add(levelPanel);
+
+        JPanel linesPanel = new JPanel();
+        linesPanel.setLayout(null);
+        linesPanel.setBounds(280,420,70,75);
+        linesPanel.setBackground(Color.GRAY);
+
+        JLabel linesLabel = new JLabel("lines", SwingConstants.CENTER);  // linesPanel에 "lines" 텍스트를 추가합니다.
+        linesLabel.setForeground(Color.WHITE);
+        linesLabel.setBounds(0, 0, 70, 20);
+        linesPanel.add(linesLabel);
+        backgroundPanel.add(linesPanel);
+
+        boardPane = new JTextPane();
+        boardPane.setEditable(false);
+        boardPane.setOpaque(false);
+        boardPane.setBorder(new TitledBorder(new LineBorder(Color.white,3)));
+        boardPane.setBounds(40,55, 220,440);
+        backgroundPanel.add(boardPane);
+
     }
+
+    JPanel backgroundPanel = new JPanel() {
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g); // 부모 클래스의 paintComponent 호출
+            g.setColor(Color.BLACK); // 색상을 검은색으로 설정
+            g.fillRect(0, 0, this.getWidth(), this.getHeight()); // 패널 전체를 검은색으로 채움
+        }
+    };
+
 
     public static void main(String[] args) {
         GameView Game = new GameView();
+        Game.setVisible(true);
     }
 }
