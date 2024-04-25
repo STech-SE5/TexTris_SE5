ac60b46b05dbdef82c988dc01ae68be8dd9095fa
Yohan Im <yohanim25@gmail.com>
2024-04-25 20:44:20 +0900

    Main 수정; Controller 생성 및 활용

--- TexTris/src/main/java/org/Stech/SE5/Main.java
+++ TexTris/src/main/java/org/Stech/SE5/Main.java
@@ -1,8 +1,25 @@
 package org.Stech.SE5;
-import org.Stech.SE5.view.HomeView;
+
+import org.Stech.SE5.controller.HomeController;
+import org.Stech.SE5.controller.Controller;
 
 public class Main {
-    public static void main(String[] args) {
-        HomeView homeView = new HomeView();
+    private static HomeController homeController;
+    private static Controller currentController;
+
+    public enum View {HOME, /*GAME, CONFIG, RECORD*/} // 추후 추가?
+
+    protected Main() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(final String[] args) {
+        /*HomeView homeView = new HomeView();
+        homeView.setVisible(true);*/
+
+        /*HomeModel.loadConfig();*/
+        homeController = new HomeController();
+        currentController = homeController;
+        currentController.setVisible(true);
     }
 }
