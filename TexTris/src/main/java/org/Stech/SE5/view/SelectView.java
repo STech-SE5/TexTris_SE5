17f73a005c9a15b9435ef14b883e6ba429606292
kms0917 <5670kms@gmail.com>
2024-05-20 22:42:59 +0900

    SelectView 생성자 추가

--- TexTris/src/main/java/org/Stech/SE5/view/SelectView.java
+++ TexTris/src/main/java/org/Stech/SE5/view/SelectView.java
@@ -1,5 +1,6 @@
 package org.Stech.SE5.View;
 
+import org.Stech.SE5.Controller.BattleController;
 import org.Stech.SE5.Controller.GameController;
 import org.Stech.SE5.Controller.HomeController;
 
@@ -22,10 +23,15 @@ public class SelectView extends JFrame {
     private boolean itemModeflag;
     private GameController gameController;
 
+    private BattleController battleController;
+
+    private boolean bBattle;
+
     public SelectView(final HomeController controller, boolean itemMode, double Size) {    // HomeController형 controller 객체를 매개로 하는 생성자
         homeController = controller;
         playerKeyListener = new PlayerKeyListener();
         itemModeflag = itemMode;
+        bBattle = false;
 
         // 전체 프로그램 or HomeView의 제목을 설정
         setTitle("SE5_TEXTRIS");
@@ -55,6 +61,60 @@ public class SelectView extends JFrame {
         JButton easyLvBtn = new JButton("Easy");
         JButton normalLvBtn = new JButton("Normal");
         JButton hardLvBtn = new JButton("Hard");
+        JButton toHomeBtn = new JButton("Back to Menu");
+        // 폰트 설정
+        Font titleFont = new Font("Arial", Font.BOLD, (int)(60 * Size));
+        Font buttonFont = new Font("Arial", Font.BOLD, (int)(24 * Size));
+
+        title.setFont(titleFont);
+        title.setBounds((int)(30 * Size), (int)(50 * Size), (int)(320 * Size), (int)(90 * Size));
+
+        // 난이도 버튼
+        setButton(easyLvBtn, buttonFont, (int)(30 * Size), (int)(200 * Size), (int)(320 * Size), (int)(60 * Size));  // 쉬움 난이도 버튼
+        setButton(normalLvBtn, buttonFont, (int)(30 * Size), (int)(270 * Size), (int)(320 * Size), (int)(60 * Size));   // 보통 난이도 버튼
+        setButton(hardLvBtn, buttonFont, (int)(30 * Size), (int)(340 * Size), (int)(320 * Size), (int)(60 * Size)); // 어려움 난이도 버튼
+        setButton(toHomeBtn, buttonFont, (int)(30 * Size), (int)(410 * Size), (int)(320 * Size), (int)(60 * Size)); // 돌아가기 버튼
+
+        this.setContentPane(bgPanel);
+        setScreen(bgPanel, title);
+
+        buttonPtrIndex = 0;
+        highlightSelectedButton();
+
+        addKeyListener(playerKeyListener); // Register key listener for this JFrame
+        setFocusable(true);
+        requestFocusInWindow();
+    }
+
+
+    public SelectView(final HomeController controller, double Size) {    // HomeController형 controller 객체를 매개로 하는 생성자
+        homeController = controller;
+        playerKeyListener = new PlayerKeyListener();
+        bBattle = true;
+
+        // 전체 프로그램 or HomeView의 제목을 설정
+        setTitle("SE5_TEXTRIS");
+
+        // FlowLayout 적용
+        FlowLayout fl = new FlowLayout();
+        fl.setAlignment(FlowLayout.RIGHT);
+        JPanel bgPanel = new JPanel(); // HomeView 내 모든 요소를 올릴 배경 패널
+
+        bgPanel.setBackground(new Color(20, 20, 20));
+        bgPanel.setLayout(null);
+        //bgPanel.setBorder(BorderFactory.createEmptyBorder(200, 0, 0, 25));
+
+        // 제목 레이블
+        JLabel title = new JLabel();
+        String titleText = null;
+
+        //updateLabelTitle(title, titleText);
+        titleText = "Battle";
+
+        // 게임 난이도 버튼
+        JButton easyLvBtn = new JButton("Basic");
+        JButton normalLvBtn = new JButton("Item");
+        JButton hardLvBtn = new JButton("Timer");
         JButton toHomeBtn = new JButton("Back to Menu");
         // 폰트 설정
         Font titleFont = new Font("Arial", Font.BOLD, (int)(60 * Size));
@@ -143,20 +203,35 @@ public class SelectView extends JFrame {
                 case KeyEvent.VK_ENTER:
                     switch(buttonPtrIndex){
                         case 0: // easyMode
-                            gameController = new GameController(itemModeflag, 0);
-                            gameController.setVisible(true);
-                            setVisible(false);
-                            break;
+                            if (bBattle){
+                                battleController = new BattleController(0);
+                                break;
+                            }else {
+                                gameController = new GameController(itemModeflag, 0);
+                                gameController.setVisible(true);
+                                setVisible(false);
+                                break;
+                            }
                         case 1: // normalMode
-                            gameController = new GameController(itemModeflag, 1);
-                            gameController.setVisible(true);
-                            setVisible(false);
-                            break;
+                            if (bBattle){
+                                battleController = new BattleController(1);
+                                break;
+                            }else {
+                                gameController = new GameController(itemModeflag, 1);
+                                gameController.setVisible(true);
+                                setVisible(false);
+                                break;
+                            }
                         case 2: // hardMode
-                            gameController = new GameController(itemModeflag, 2);
-                            gameController.setVisible(true);
-                            setVisible(false);
-                            break;
+                            if (bBattle){
+                                battleController = new BattleController(2);
+                                break;
+                            }else {
+                                gameController = new GameController(itemModeflag, 2);
+                                gameController.setVisible(true);
+                                setVisible(false);
+                                break;
+                            }
                         case 3: // backToMainMenu
                             homeController.setVisible(true);
                             setVisible(false);
